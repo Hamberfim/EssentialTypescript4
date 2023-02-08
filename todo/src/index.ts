5abd725801f64f1622a22cf8c5ed02159dc5ca1e
Hamberfim <adhamlingithub@gmail.com>
2023-02-08 13:33:37 -0600

    listings 1.22- 1.24

--- todo/src/index.ts
+++ todo/src/index.ts
@@ -13,14 +13,16 @@ let collection: TodoCollection = new TodoCollection("Anthony", todos);
 console.clear();
 console.log(`${collection.userName}'s Todo List`);
 
-// todos.forEach((element) => {
-//   // console.log(JSON.stringify(element));
-//   element.printDetails();
-// });
+todos.forEach((element) => {
+  // console.log(JSON.stringify(element));
+  element.printDetails();
+});
 
 // let newId: number = collection.addTodo("Walk the dog");
 // let todoItem: TodoItem = collection.getTodoById(newId);
 // todoItem.printDetails();
 
 // console.log(JSON.stringify(todoItem));
+console.log(`\n=== Only Completed ===`);
+collection.removeComplete();
 collection.getTodoItems(true).forEach((item) => item.printDetails());
